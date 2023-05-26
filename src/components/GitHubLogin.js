8cf3ee1f8cb5cbe84c6d82f86a6e102851da6c5c
Tiago <100311071+tiagopraca@users.noreply.github.com>
2023-05-26 15:46:02 +0100

    Fixed errors

--- src/components/GitHubLogin.js
+++ src/components/GitHubLogin.js
@@ -1,24 +1,40 @@
-import React, { useState } from 'react';
+// GitHubLogin.js
 
-const GithubLogin = () => {
-    const setErrorMessage = useState('');
+import React from 'react';
 
-    const handleGithubLogin = async () => {
-        try {
-          document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-          window.location.href = 'http://localhost:3000/users/auth/github';
-        } catch (error) {
-          console.log(error);
-          // Handle the error
-          setErrorMessage('An error occurred while initiating GitHub authentication.');
-        }
-      };
+const GitHubLogin = ({ onClose }) => {
+  const handleGithubLogin = async () => {
+    try {
+      document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+      window.location.href = 'http://localhost:3000/users/auth/github'; // Replace with your backend URL
+    } catch (error) {
+      console.log(error);
+      // Handle the error
+    }
+  };
 
   return (
-    <div>
-      <button onClick={handleGithubLogin}>Login with GitHub</button>
+    <div
+      style={{
+        position: 'fixed',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        backgroundColor: 'white',
+        padding: '20px',
+        zIndex: 9999,
+      }}
+    >
+      <h2>Login Box</h2>
+      {/* Add your login box content here */}
+      <button type="button" onClick={handleGithubLogin}>
+        Login with GitHub
+      </button>
+      <button type="button" onClick={onClose}>
+        Close
+      </button>
     </div>
   );
 };
 
-export default GithubLogin;
+export default GitHubLogin;
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,32 +1,50 @@
-// HomePage.js
-
 import React from 'react';
-import { useHistory } from 'react-router-dom';
+import GitHubLogin from './GitHubLogin';
 
 const HomePage = () => {
-  const history = useHistory();
-  const [redirectToLogin, setRedirectToLogin] = React.useState(false);
+  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+  const [showLoginBox, setShowLoginBox] = React.useState(false);
 
   const handleGoToLogin = () => {
-    setRedirectToLogin(true);
+    setShowLoginBox(true);
+  };
+
+  const handleCloseLoginBox = () => {
+    setShowLoginBox(false);
   };
 
   React.useEffect(() => {
-    if (redirectToLogin) {
-      history.push('/github');
-      window.location.reload()
-    }
-  }, [redirectToLogin, history]);
+    const checkLoginStatus = async () => {
+      try {
+        const response = await fetch('http://localhost:3000/users/check-login');
+
+        if (response.ok) {
+          const data = await response.json();
+          setIsLoggedIn(data.isLoggedIn);
+        } else {
+          setIsLoggedIn(false);
+        }
+      } catch (error) {
+        console.log(error);
+        setIsLoggedIn(false);
+      }
+    };
+
+    checkLoginStatus();
+  }, []);
 
   return (
     <div>
-      <h1>Welcome to the GAS !</h1>
-      <button type="button" onClick={handleGoToLogin}>
-          Login
-      </button>
+      <h1>Welcome to the GAS!</h1>
       {/* Add your home page content here */}
+      {showLoginBox && !isLoggedIn && <GitHubLogin onClose={handleCloseLoginBox} />}
+      {!isLoggedIn && (
+        <button type="button" onClick={handleGoToLogin}>
+          Login
+        </button>
+      )}
     </div>
   );
-}
+};
 
 export default HomePage;
